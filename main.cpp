dd1cdefd6ec60581ee6ef83511cbf4c4e9c13703
ayagamalmh98 <48804613+ayagamalmh98@users.noreply.github.com>
2020-05-30 18:11:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -251,7 +251,7 @@ public:
   struct preobj extract(vector<string> data) {
         struct preobj info;
         string Label, Operator, Operand;
-        if (data.size() == 1||( data.size()==2&&(data.at(1)=="FIX" || data.at(1) == "FLOAT" || data.at(1) == "HIO" || data.at(1) == "NORM" || data.at(1) == "SIO" || data.at(1) == "TIO" )   )  ) {
+         if ((data.size() == 1&&data.at(0)!="END"&& data.at(0) != "")||( data.size()==2&&(data.at(1)=="FIX" || data.at(1) == "FLOAT" || data.at(1) == "HIO" || data.at(1) == "NORM" || data.at(1) == "SIO" || data.at(1) == "TIO" )   )  ) {
             cout << "Format 1 not supported\n";
             end = 1;
             return info;
